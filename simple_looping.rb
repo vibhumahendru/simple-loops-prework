82c6272896ea4b78cd57bdad455174c91a2effce
vibhu mahendru <vibhumahendru@gmail.com>
2018-10-02 18:23:01 +0000

    Done.

--- simple_looping.rb
+++ simple_looping.rb
@@ -1,28 +1,32 @@
 # REMEMBER: print your output to the terminal using 'puts'
 
 def loop_iterator(number_of_times)
-  # Code your solution here using the "loop" keyword to puts out the below phrase
-  # This and all the other methods take an argument of an integer 
-  # The integer is the number of times the loops should puts out the phrase
-  # How can we make sure the loop breaks once it has puts out the phrase the 
-  #   correct number of times?
-  # Maybe we should keep count of the number of times we've puts out the 
-  #  phrase and break when the counter hits the appropriate number...
-  phrase = "Welcome to Flatiron School's Web Development Course!"
   
+  phrase = "Welcome to Flatiron School's Web Development Course!"
+   x = 0
+   loop do 
+    if x<number_of_times 
+      puts phrase
+     x+=1
+   end
+ end
 end
 
 def times_iterator(number_of_times)
   # code your solution here using the "times" keyword
   phrase = "Welcome to Flatiron School's Web Development Course!"
-  
+  number_of_times.times do
+    puts phrase
+  end
 end
 
 def while_iterator(number_of_times)
-  # code your solution here using the "while" keyword
-  # hint: user a counter to tell the while loop when to stop!
   phrase = "Welcome to Flatiron School's Web Development Course!"
-  
+  x = 0 
+  while x<number_of_times
+  puts phrase
+  x+=1
+end
 end
 
 def until_iterator(number_of_times)
