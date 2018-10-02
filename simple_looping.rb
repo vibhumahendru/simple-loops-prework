0203c3d9745148a2b7d7715e347113e033c15e5e
vibhu mahendru <vibhumahendru@gmail.com>
2018-10-02 18:26:51 +0000

    automatically backed up by learn

--- simple_looping.rb
+++ simple_looping.rb
@@ -3,7 +3,12 @@
 def loop_iterator(number_of_times)
   
   phrase = "Welcome to Flatiron School's Web Development Course!"
-  
+  x = 0
+  loop do 
+    if x  number_of_times
+      puts phrase
+      x+=1
+    end
 end
 
 def times_iterator(number_of_times)
